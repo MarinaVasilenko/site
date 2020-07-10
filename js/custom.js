8e35ef4fa174e901d809d574996f61fb25b265d6
Marina <mv280693@gmail.com>
2020-07-10 19:56:01 +0300

    add site

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+function initMap() {
+    var uluru = {lat: 50.5254402, lng: 30.519902};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+
+    google.maps.event.addDomListener(window, "resize", function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+}
